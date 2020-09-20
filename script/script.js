df4295c80dd5bed5f259e440d9c0a05945263bdb
washopilot <washopilot@yahoo.com>
2020-09-20 11:32:35 -0500

    commit inicial

---
+++ script/script.js
@@ -0,0 +1,39 @@
+console.log('Está funcionando...');
+
+let theme = localStorage.getItem('theme');
+
+if (theme == null) {
+  setTheme('light');
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+for (var i = 0; themeDots.length > 1; i++) {
+  themeDots[i].addEventListener('click', function () {
+    let mode = this.dataset.mode;
+    console.log('Opción CLICK', mode);
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == 'light') {
+    document.getElementById('theme-style').href = 'styles/default.css';
+  }
+
+  if (mode == 'blue') {
+    document.getElementById('theme-style').href = 'styles/blue.css';
+  }
+
+  if (mode == 'green') {
+    document.getElementById('theme-style').href = 'styles/green.css';
+  }
+
+  if (mode == 'purple') {
+    document.getElementById('theme-style').href = 'styles/purple.css';
+  }
+
+  localStorage.setItem('theme', mode);
+}
